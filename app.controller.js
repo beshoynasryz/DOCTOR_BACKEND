fc1cc8aeddbb42ab2707ff25603a39dbe4630217
beshoynasryz <beshoynasry1@gmail.com>
2025-02-01 21:14:04 +0200

    middlware auth 🚀

--- app.controller.js
+++ app.controller.js
@@ -1,15 +1,16 @@
 import connectCloudinary from "./config/cloudinary.js"
 import connectDB from "./config/dbConnection.js"
 import adminRouter from "./modules/admin/admin.controller.js"
+import errorHandler from "./middlewares/errorHandlerMiddleware.js"
 function bootStrap(app, express) {
     connectDB()
     connectCloudinary()
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
-
     //Routes 
     app.use('/api/admin',adminRouter)
-    
+
+    app.use(errorHandler)
     app.use('/',(req,res,next)=>{
         res.send('Welcome to the Doctor app');
     })
--- modules/admin/admin.service.js
+++ modules/admin/admin.service.js
@@ -1,11 +1,44 @@
 import doctorModel from "../doctor/doctor.schema.js"
 import asyncHandler from "express-async-handler"
+import bcrypt from "bcrypt"
+import {v2 as cloudinary} from "cloudinary"
+import { json } from "express"
+
 
 
 export const addDoctor = asyncHandler(async (req, res , next) => {
-    const {name , email , password  , speciality , degree , experience , about  , fees , address} = req.body;
+    const {name , email , password  , speciality , degree , experience , about  , fees , address } = req.body;
     const imageFile = req.file
-  
 
+    if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address || !imageFile) {
+       return res.status(400).json({message: "All fields are required"}) 
+    }
 
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    //upload image to cloudinary
+
+    const uploadImage = await cloudinary.uploader.upload(imageFile.path, {responseType: "image"})
+    const imageUrl = uploadImage.secure_url
+    
+    const doctorData = {
+        name,
+        email,
+        password:  hashPassword,
+        speciality,
+        degree,
+        experience,
+        about,
+        fees,
+        address: JSON.parse(address),
+        image: imageUrl,
+        date : Date.now()
+    }
+
+    const doctor = new doctorModel(doctorData)
+    await doctor.save()
+    res.status(201).json({success: true , message: "Doctor added successfully" , doctor } )
 })
+
+    
+
